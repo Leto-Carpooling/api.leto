6479cd3c91bbca6367db8d741f3fa84f81e90f5d
levizwannah <levizwannah@gmail.com>
2021-08-24 21:33:34 +0300

    approval status check for driver done

--- OT_server/src/driver/checkApproval.php
+++ OT_server/src/driver/checkApproval.php
@@ -3,21 +3,10 @@
 
     //$newDriver is already initialized in the master.inc.php
 
-    $approved = $newDriver->getApprovalStatus() == "approved";
-
-    if(!$approved){
-        exit(
-            Response::makeResponse(
-                "NA",
-                "Your application has not yet been approved"
-            )
-        );
-    }
-
     exit(
         Response::makeResponse(
             "OK",
-            "You are now a driver on Leto."
+            $newDriver->getApprovalStatus()
         )
     );
 
